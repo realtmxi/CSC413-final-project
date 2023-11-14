a4a6506da90436d424d9af311d9dc64c461dab41
Ethan <ethanq918@163.com>
2023-11-13 23:24:42 -0500

    first commit of EDA

--- data/EDA.py
+++ data/EDA.py
@@ -6,16 +6,54 @@ csv_file_path = 'blkjckhands.csv'
 
 df = pd.read_csv(csv_file_path)
 
-df2 = df['sumofcards']
+df1 = df['sumofdeal']
+value_counts = df1.value_counts()
+value_counts = value_counts.sort_index()
+plt.subplot(2, 2, 1)
+value_counts.plot(kind='bar', edgecolor='black', figsize=(10, 6))
 
-value_counts1 = df2.value_counts()
-
-value_counts1 = value_counts1.sort_index()
-
-value_counts1.plot(kind='bar', edgecolor='black', figsize=(10, 6))
-
-plt.title('Distribution of Values of Each Hand')
+plt.title('Distribution of Values of Each Hand of Dealer')
 plt.xlabel('Unique Values of Each Hand')
 plt.ylabel('Frequency')
+# plt.show()
 
+
+df2 = df['sumofcards']
+value_counts1 = df2.value_counts()
+value_counts1 = value_counts1.sort_index()
+plt.subplot(2, 2, 2)
+value_counts1.plot(kind='bar', edgecolor='black', figsize=(10, 6))
+
+plt.title('Distribution of Values of Each Hand of Player')
+plt.xlabel('Unique Values of Each Hand')
+plt.ylabel('Frequency')
+# plt.show()
+
+
+df3 = df['ply2cardsum']
+value_counts2 = df3.value_counts()
+value_counts2 = value_counts2.sort_index()
+plt.subplot(2, 2, 3)
+value_counts2.plot(kind='bar', edgecolor='black', figsize=(10, 6))
+
+plt.title('Distribution of the Sum of First Two Cards of Player')
+plt.xlabel('Unique Values of Each Sum')
+plt.ylabel('Frequency')
+# plt.show()
+
+
+df4 = df['winloss']
+plt.subplot(2, 2, 4)
+value_counts3 = df4.value_counts()
+value_counts3 = value_counts3.sort_index()
+value_counts3.plot(kind='bar', edgecolor='black', figsize=(10, 6))
+
+plt.title('Distribution for Results of Each Hand for Player')
+plt.xlabel('Each outcome')
+plt.ylabel('Frequency')
+
+plt.tight_layout()
+plt.savefig('bar_hand.png')
 plt.show()
+
+
