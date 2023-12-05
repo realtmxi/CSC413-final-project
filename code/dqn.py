b9e7304712c5a9dc4e6c5ec661d8ca540eb9a5e7
Ethan <ethanq918@163.com>
2023-12-04 21:44:21 -0500

    first commit of ppo

--- code/dqn.py
+++ code/dqn.py
@@ -1,1 +1,13 @@
 # using Deep Q Learning Methods on balckjack Agent
+
+import gym
+import time
+
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.distributions import MultivariateNormal
+
+
